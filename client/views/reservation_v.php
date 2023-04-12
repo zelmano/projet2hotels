5d6b80b0c609829e00ba7b1aea0f83a600dda000
zelmano <zelman.brun@gmail.com>
2023-04-12 22:04:22 +0200

    veu changes

--- client/views/reservation_v.php
+++ client/views/reservation_v.php
@@ -1,27 +1,19 @@
 <html>
 <head>
+  <meta charset="utf-8">
   <title>Reservations</title>
+  <link rel="stylesheet" href="/client/scripts/style.css">
 </head>
 <body>
   <h1>Reservations</h1>
-  <table>
-    <tr>
-      <th>Reservation ID</th>
-      <th>Customer ID</th>
-      <th>Room ID</th>
-      <th>Check In</th>
-      <th>Check Out</th>
-      <th>Price</th>
-    </tr>
-    <?php foreach ($reservations as $reservation): ?>
-      <tr>
-        <td><?php echo $reservation['nom_client']; ?></td>
-        <td><?php echo $reservation['id_chambre']; ?></td>
-        <td><?php echo $reservation['date_arrivee']; ?></td>
-        <td><?php echo $reservation['date_fin']; ?></td>
-        <td><?php echo $reservation['id_sejour']; ?></td>
-      </tr>
-    <?php endforeach; ?>
-  </table>
+  <div>
+    <div class="room_card">
+      Nom hotel
+      Prix
+      Machin
+      Truc
+      bouton réserver
+    </div>
+  </div>
 </body>
 </html>
