b0a02bbed448877d6dfa558858d26c776c342ba0
zelmano <zelman.brun@gmail.com>
2023-05-03 23:09:14 +0200

    mm push qu'avant pour les categories

--- client/controllers/reservation_c.php
+++ client/controllers/reservation_c.php
@@ -2,7 +2,8 @@
 require_once('../models/reservation_m.php');
 
 $reservations = Reservation::getReservations();
-$hotels=Reservation::getHotels();//z
+$hotels=Reservation::getHotels();//zel
+$categories=Reservation::getCategories();//zel
 
 require_once('../views/reservation_v.php');
 
