2467d099d4965387654bb84a6e33c98eec5615fd
zelmano <zelman.brun@gmail.com>
2023-05-03 22:44:45 +0200

    ajout du get de hotels

--- client/controllers/reservation_c.php
+++ client/controllers/reservation_c.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once('../models/Reservation.php');
+require_once('../models/reservation_m.php');
 
 $reservations = Reservation::getReservations();
-$hotels= Reservation::getHotels();//z
+$hotels=Reservation::getHotels();//z
 
 require_once('../views/reservation_v.php');
 
