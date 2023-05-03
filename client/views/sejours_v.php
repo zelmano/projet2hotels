e02e16cc87864fee43f1707df11c65dfd80f7d31
zelmano <zelman.brun@gmail.com>
2023-05-03 23:27:13 +0200

    squelette de base

--- client/views/sejours_v.php
+++ client/views/sejours_v.php
@@ -5,6 +5,10 @@
   <link rel="stylesheet" href="/client/scripts/style.css">
 </head>
 <body>
-  
+  <h1>Mes Séjours</h1>
+  <h2>Recherche</h2>
+  <p>Vous pouvez consulter les disponibilités des chambres d'un de vos séjours en cours ou passé.</p>
+
+  <!-- pas de formulaire, seulement afficher la liste des séjours réservés -->
 </body>
 </html>
