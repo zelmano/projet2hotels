e9e127b87ada4b3a4bbc81906833880bb6855e4b
zelmano <zelman.brun@gmail.com>
2023-05-04 09:34:23 +0200

    ajout todo variables manquantes

--- client/controllers/reservation_c.php
+++ client/controllers/reservation_c.php
@@ -5,6 +5,9 @@ $reservations = Reservation::getReservations();
 $hotels=Reservation::getHotels();//zel
 $categories=Reservation::getCategories();//zel
 
+//ce qu'il faut encore
+//$nom_client : à récupérer après la connexion
+
 require_once('../views/reservation_v.php');
 
 ?>
