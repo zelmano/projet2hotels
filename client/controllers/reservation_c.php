a0cd2a2c52347526fc6b4dca233896892c5ba6d5
zelmano <zelman.brun@gmail.com>
2023-05-04 09:52:21 +0200

    formalisme $chambre

--- client/controllers/reservation_c.php
+++ client/controllers/reservation_c.php
@@ -8,6 +8,14 @@ $categories=Reservation::getCategories();//zel
 //ce qu'il faut encore
 //$nom_client : à récupérer après la connexion
 
+//$chambres qui contient les chambres disponibles pour le séjour sélectionné
+//$chambres est un tableau qui s'organise comme ça :
+// $chambres = array(
+  //     array('id chambre', 'hotel', 'categorie', 'prix'),
+  //     array('id chambre', 'hotel', 'categorie', 'prix'),
+  //     array('id chambre', 'hotel', 'categorie', 'prix'),
+  // etc...
+
 //pour info, voilà les id du formulaire à aller cherche dans $_POST :
 //hotel
 //denomination
--- client/views/reservation_v.php
+++ client/views/reservation_v.php
@@ -2,52 +2,114 @@
 <head>
   <meta charset="utf-8">
   <title>Reservations</title>
-  <link rel="stylesheet" href="/client/scripts/style.csss">
+  <link rel="stylesheet" href="/client/scripts/style.css">
 </head>
 <body>
-  <h1>Rechercher une chambre</h1>
-  <h2>Recherche</h2>
-  <p>Vous pouvez rechercher une chambre en fonction de l'hotel, du type de chambre et des dates de début et de fin.</p>
-  <p>Si vous ne renseignez pas de date, la recherche se fera sur toutes les dates.</p>
-  <p>Si vous ne renseignez pas de type de chambre, la recherche se fera sur tous les types de chambre.</p>
-  <p>Si vous ne renseignez pas d'hotel, la recherche se fera sur tous les hotels.</p>
-  <p>Si vous ne renseignez pas de date de début, la recherche se fera sur toutes les dates de début.</p>
-  <p>Si vous ne renseignez pas de date de fin, la recherche se fera sur toutes les dates de fin.</p>
 
-  <h2>Formulaire</h2>
+  <nav>
+    <div id="nav_header">
+      <img src="../sources/hotel.svg" alt="logo">
+      <h1>Les Hotels</h1>
+    </div>
+    <div id="nav_user">
+      <h1 id="nav_username">$nom_client</h1> <!-- $nom_client -->
+      <img src="../sources/profile.svg" alt="profile">
+    </div>
+    <div id="nav_tabs">
+      <div class="nav_tab">
+        <img src="../sources/home.svg" alt="accueil">
+        <a href="accueil.html">Accueil</a>
+      </div>
+      <div class="nav_tab">
+        <img src="../sources/plane.svg" alt="avion">
+        <a href="reservation_c.php">Réserver</a>
+      </div>
+      <div class="nav_tab">
+        <img src="../sources/list.svg" alt="list">
+        <a href="sejours_c.php">Mes Séjours</a>
+      </div>
+    </div>
+  </nav>
 
-  <form action="" method="post">
+  <main>
 
-    <!--séléction de l'hotel-->
-    <label for="hotel">Hotel</label>
-    <select name="hotel" id="hotel">
+    <h2>Recherche</h2>
+
+    <!-- <p>Vous pouvez rechercher une chambre en fonction de l'hotel, du type de chambre et des dates de début et de fin.</p>
+    <p>Si vous ne renseignez pas de date, la recherche se fera sur toutes les dates.</p>
+    <p>Si vous ne renseignez pas de type de chambre, la recherche se fera sur tous les types de chambre.</p>
+    <p>Si vous ne renseignez pas d'hotel, la recherche se fera sur tous les hotels.</p>
+    <p>Si vous ne renseignez pas de date de début, la recherche se fera sur toutes les dates de début.</p>
+    <p>Si vous ne renseignez pas de date de fin, la recherche se fera sur toutes les dates de fin.</p> -->
+
+    <form action="" method="post">
+
+      <!--séléction de l'hotel-->
+      <div class="form_selector">
+        <label for="hotel">Hotel</label>
+        <select name="hotel" id="hotel">
+          <?php
+          //on récupère la liste des hotels et on les parcourt pour créer les différentes options de recherche
+          for ($i=0;$i<count($hotels);$i++){
+            echo '<option value="'.$hotels[$i]['nom'].'">'.$hotels[$i]['nom'].'</option>';
+          }
+          ?>
+        </select>
+      </div>
+
+      <!--séléction de la catégorie de chambre-->
+      <div class="form_selector">
+        <label for="categorie">Catégorie</label>
+        <select name="categorie" id="categorie">
+          <?php
+          //on récupère la liste des dénominations des categories de chambre et on les parcourt pour créer les différentes options de recherche
+          for ($i=0;$i<count($categories);$i++){
+            echo '<option value="'.$categories[$i]['denomination'].'">'.$categories[$i]['denomination'].'</option>';
+          }
+          ?>
+        </select>
+      </div>
+
+      <!-- séléction de la date de début et de fin -->
+      <div class="form_selector">
+        <label for="datedebut">Date de début</label>
+        <input type="date" name="datedebut" id="datedebut">
+      </div>
+
+      <div class="form_selector">
+        <label for="datefin">Date de fin</label>
+        <input type="date" name="datefin" id="datefin">
+      </div>
+
+      <input type="submit" value="Rechercher">
+    </form>
+
+
+    <div class="cards">
+
       <?php
-      //on récupère la liste des hotels et on les parcourt pour créer les différentes options de recherche
-      for ($i=0;$i<count($hotels);$i++){
-        echo '<option value="'.$hotels[$i]['nom'].'">'.$hotels[$i]['nom'].'</option>';
+      //on parcourt la liste des chambres disponibles et on les affiche
+      for ($i=0;$i<count($chambres);$i++){
+        echo '<div class="room_card">';
+        echo '<h2>'.$chambres[$i]['nom'].'</h2>';
+        echo '<table>';
+        echo '<tr>';
+        echo '<th>catégorie</th>';
+        echo '<th>type</th>';
+        echo '</tr>';
+        echo '<tr>';
+        echo '<td>'.$chambres[$i]['denomination'].'</td>';
+        echo '<td>'.$chambres[$i]['type'].'</td>';
+        echo '</tr>';
+        echo '</table>';
+        echo '<button class="btn">Reserver</button>';
+        echo '</div>';
       }
       ?>
-    </select>
 
-    <!--séléction de la catégorie de chambre-->
-    <label for="categorie">Catégorie</label>
-    <select name="categorie" id="categorie">
-      <?php
-      //on récupère la liste des dénominations des categories de chambre et on les parcourt pour créer les différentes options de recherche
-      for ($i=0;$i<count($categories);$i++){
-        echo '<option value="'.$categories[$i]['denomination'].'">'.$categories[$i]['denomination'].'</option>';
-      }
-      ?>
-    </select>
+    </div>
 
-    <label for="datedebut">Date de début</label>
-    <input type="date" name="datedebut" id="datedebut">
-
-    <label for="datefin">Date de fin</label>
-    <input type="date" name="datefin" id="datefin">
-
-    <input type="submit" value="Rechercher">
-  </form>
+  </main>
 
 
   <!-- <div>
