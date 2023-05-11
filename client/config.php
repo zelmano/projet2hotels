23caf375ff84f5b1054c1dab14bd280ac274d4d6
zelmano <118254919+zelmano@users.noreply.github.com>
2023-05-11 13:58:44 +0200

    Update config.php avec les adresses du serv

--- client/config.php
+++ client/config.php
@@ -1,7 +1,7 @@
 <?php
-const DB_USER = 'projet2hotels';
-const DB_PASSWORD = 'isen14';
-const DB_NAME = 'projet2hotels';
+const DB_USER = 'cir2';
+const DB_PASSWORD = 'Isen@2';
+const DB_NAME = 'cir2';
 const DB_SERVER = '127.0.0.1';
 const DB_PORT = '5432';
 ?>
